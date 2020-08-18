1cd7bc6cd5f250a9f1b03ce4cbdbc14aacf783e4
Diego Ruidajo <2630308+diegoruidajo@users.noreply.github.com>
2020-08-17 22:45:21 -0500

    Create fondoRuidajo2.js

---
+++ scripts/fondoRuidajo2.js
@@ -0,0 +1,21 @@
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.position(0, 0);
+  canvas.style('z-index', '-1');
+
+  background(0);
+  strokeWeight(20);
+  frameRate(60);
+}
+
+function draw() {
+  for (let i = 0; i < width; i++) {
+    let r = random(1000);
+    stroke(r);
+    line(i, 0, i, height);
+  }
+}
